ab0695e690aaa13a5e00c34c98b363ec11ab10b4
Pim van de Ven <pimda_2@hotmail.com>
2014-10-30 21:20:53 +0100

    made a start to a moving backdrop

--- framework/src/Controller/Time.hs
+++ framework/src/Controller/Time.hs
@@ -6,26 +6,35 @@ module Controller.Time (
 ) where
 
 import Control.Arrow ((>>>))
-
 import Data.List
-
 import System.Random
-
+import Graphics.Gloss
 import Model
 
 -- | Time handling
 
 timeHandler :: Float -> World -> World
-timeHandler time world@(World {..}) = world { currentTime = time, heading = newHeading heading rotateAction, location = newLocation location heading}
-		
+timeHandler time world@(World {..}) = world { currentTime = time,
+                                              heading = newHeading heading rotateAction,
+                                              location = newLocation location movementSpeed heading,
+											  backdrop = moveStars (addStar backdrop rndGen)}
+
 newHeading :: Float -> RotateAction -> Float
 newHeading r RotateLeft  = r - rotateSpeed
 newHeading r RotateRight = r + rotateSpeed
 newHeading r _           = r
 
-newLocation :: Location -> Float -> Location
-newLocation (x,y) r = ((sin rad) * movementSpeed + x, (cos rad) * movementSpeed + y)
+newLocation :: Location -> Float -> Float -> Location --location, speed, heading
+newLocation (x,y) s r = ((sin rad) * s + x, (cos rad) * s + y)
     where rad = r / 360  * (2 * pi)
+
+addStar :: [Particle] -> StdGen -> [Particle]
+addStar x rnd = (Particle white 10 90 2 (100, ypos)) : x
+    where ypos = fst (randomR (-100, 100) rnd)
+
+moveStars :: [Particle] -> [Particle]
+moveStars x = map moveStar x
+    where moveStar part@(Particle {..}) = part { loc = newLocation loc speed head }
 
 rotateSpeed :: Float
 rotateSpeed = 2
--- framework/src/View.hs
+++ framework/src/View.hs
@@ -13,10 +13,16 @@ import Model
 
 draw :: Float -> Float -> World -> Picture
 draw horizontalResolution verticalResolution world@(World{..})
-    = Pictures [Blank, setToPos location heading ship]
+    = Pictures ([Blank] ++ drawStars backdrop ++ [setToPos location heading ship])
 
 ship :: Picture
 ship = Color red (Polygon [(0, 20), (-10, 0), (0, 2), (10, 0)])
+
+drawStars stars = map drawStar stars
+
+drawStar star@(Particle {..}) = setToPos loc head (Color color (starShape size))
+
+starShape size = Circle size
 
 setToPos :: Location -> Float -> Picture -> Picture
 setToPos (x, y) r p = Translate x y (Rotate r p)
